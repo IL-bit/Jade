ae275f2b5154c075fa038f94d68a1fc350d78525
Ilia Vlasov <vila6365@gmail.com>
2024-11-17 20:16:22 +0300

    build

--- src/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-export const prevPage = createAction('BACK');
-export const nextPage = createAction('NEXT');
-export const changePage = createAction('CHANGE');
-export const togglePopup = createAction('TOGGLEPOPUP');
